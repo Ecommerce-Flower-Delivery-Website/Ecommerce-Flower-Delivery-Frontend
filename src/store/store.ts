1e10318e899377205f2e7a681fc0cf7c9181797c
mootaz20 <mootazalhalak695@gmail.com>
2025-01-02 14:57:53 +0200

    connect-products

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,11 +4,13 @@ import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { authSliceReducer } from "./slices/authSlice";
 import { useDispatch } from "react-redux";
 import { orderReducer } from "./slices/orderSlice";
+import  productReducer  from "./slices/productSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSliceReducer,
     orders: orderReducer,
+    product: productReducer 
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
